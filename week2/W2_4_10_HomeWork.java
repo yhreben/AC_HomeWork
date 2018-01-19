7478e6f164e06222282d398c49855d5407aa6f03
Yurii <grjuve@gmail.com>
2018-01-20 00:41:28 +0200

    Add W2_4_10_HomeWork

---
+++ week2/W2_4_10_HomeWork.java
@@ -0,0 +1,35 @@
+package week2;
+
+import Utils.ArrayUtils;
+
+/*
+ * Написать метод, который будет обрезать массив по границам start и end
+ * */
+
+public class W2_4_10_HomeWork {
+    public static void main(String[] args) {
+
+        int sizeOfArray = 20;
+
+        int[] array = ArrayUtils.generateArray(sizeOfArray);
+        ArrayUtils.printArray(array);
+
+        int[] splittedArray = splitArray(array, 5, 10);
+        ArrayUtils.printArray(splittedArray);
+
+    }
+
+
+    public static int[] splitArray(int[] arr, int start, int end) {
+
+        int outputArrayLength = end + 1 - start;
+
+        int[] newArray = new int[outputArrayLength];
+
+        for (int i = start; i <= end; i++) {
+
+            newArray[i - start] = arr[i];
+        }
+        return newArray;
+    }
+}
