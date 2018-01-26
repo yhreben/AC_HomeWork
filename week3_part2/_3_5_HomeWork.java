6e8c6b8eabaff27b44b8bc345322c8c49a27cd9b
Yurii <grjuve@gmail.com>
2018-01-27 00:10:22 +0200

    Update HM

--- week3_part2/_3_5_HomeWork.java
+++ week3_part2/_3_5_HomeWork.java
@@ -26,28 +26,80 @@ public class _3_5_HomeWork {
         System.out.println("Our matrix:");
         ArrayUtils.printArray(matrix);
 
-        int minRowIndex = findMinRowIndex(matrix);
+        //int minRowIndex = findMinRowIndex(matrix);
+        findMinRowIndex(matrix);
+
+        ArrayUtils.printArray(matrix);
 
     }
 
-    public static int findMinRowIndex(int[][] matrix) {
+    public static void findMinRowIndex(int[][] matrix) {
 
-        int rowIndex = 0;
+        int minRowIndex = 0;
+        int maxRowIndex = 0;
         int rowSum;
-        int columnSum;
+        int minRowSum = 0;
+        int maxRowSum = 0;
+//        Create array to get sum of columns
+        int [] array = new int[matrix.length];
+//        int columnSum;
 
         for (int i = 0; i < matrix.length; i++) {
             rowSum = 0;
-            columnSum = 0;
+//            columnSum = 0;
             for (int j = 0; j < matrix[i].length; j++) {
                 rowSum += matrix[i][j];
-                columnSum += matrix[j][i];
+//                In this array we'll have sum of matrix columns
+                array [j] += matrix[i][j];
+//                columnSum += matrix[j][i];
+            }
+//                Save min and max row sum
+            if (i == 0) {
+                minRowSum = rowSum;
+                maxRowSum = rowSum;
+            }
+//            Find min row index
+            if (minRowSum > rowSum){
+                minRowSum = rowSum;
+                minRowIndex = i;
+            }
+//            Find max row index
+            if (maxRowSum < rowSum){
+                maxRowSum = rowSum;
+                maxRowIndex = i;
             }
 
             System.out.println("RowIndex = " + i + "; Summa = "  + rowSum);
-            System.out.println("ColumnIndex = " + i + "; Summa = "  + columnSum);
+//            System.out.println("ColumnIndex = " + i + "; Summa = "  + columnSum);
         }
-        return rowIndex;
+        ArrayUtils.printArray(array);
+        System.out.println("minRowIndex = " + minRowIndex + "; minRowSum = " + minRowSum);
+        System.out.println("maxRowIndex = " + maxRowIndex + "; msxRowSum = " + maxRowSum);
+
+        replaceRowsInMatrix(matrix, minRowIndex, maxRowIndex);
+        //replaceColumnsInMatrix(matrix, 0, 4);
     }
+
+    public static void replaceRowsInMatrix(int[][] matrix, int minRowIndex, int maxRowIndex) {
+
+        int[] minIndexArray = matrix[minRowIndex];
+
+        matrix[minRowIndex] = matrix[maxRowIndex];
+        matrix[maxRowIndex] = minIndexArray;
+
+    }
+
+    public static void replaceColumnsInMatrix(int[][] matrix, int minColumnIndex, int maxColumnIndex) {
+
+        int columnValue;
+        for (int i = 0; i < matrix.length; i++) {
+
+            columnValue = matrix[i][minColumnIndex];
+            matrix[i][minColumnIndex] = matrix[i][maxColumnIndex];
+            matrix[i][maxColumnIndex] = columnValue;
+            }
+
+        }
+
 
 }
